d507635a9ad4797b785035daac9f5b7cd45e675d
duncdrum <d.paterson@me.com>
2025-06-27 16:52:14 +0200

    test(e2e): ignore 3rd party error

--- test/cypress/support/e2e.js
+++ test/cypress/support/e2e.js
@@ -33,3 +33,13 @@ Cypress.on('uncaught:exception', (err, runnable) => {
     return false
   }
 })
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // we expect a 3rd party library error with message 'trimmed is undefined'
+  // and don't want to fail the test so we return false
+  // see #4
+  if (err.message.includes('Cannot read properties of undefined ')) {
+    return false
+  }
+})
+
